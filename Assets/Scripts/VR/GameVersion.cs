717aeb070726ae6fa6f39dffc34975d87fadcba6
EnderSkippy <111462993+EnderSkippy@users.noreply.github.com>
2022-12-04 19:24:05 -0500

    Version Update to 0.3
    -Adds 0.3 to better indicate what version of the mod you're playing.

--- Assets/Scripts/VR/GameVersion.cs
+++ Assets/Scripts/VR/GameVersion.cs
@@ -1,6 +1,6 @@
 public static class GameVersion
 {
     public const string gameName = "Reel to Real";
-    public const string gameVersion = "1.62.2 VR";
+    public const string gameVersion = "1.62.3 (Enhanced Mod 0.3)";
     public const string isVR = "false";
 }
