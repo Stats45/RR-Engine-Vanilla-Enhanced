2fc6502c506e42ee69377ced56a1640b81d0d1d2
EnderSkippy <111462993+EnderSkippy@users.noreply.github.com>
2023-02-26 19:07:59 -0500

    Last Showtape Fix

--- Assets/Scripts/Simulation/UI_ShowtapeManager.cs
+++ Assets/Scripts/Simulation/UI_ShowtapeManager.cs
@@ -247,7 +247,7 @@ public class UI_ShowtapeManager : MonoBehaviour
                         }
 
                         //Check if show is over
-                        if ((!useVideoAsReference && referenceSpeaker.time >= (speakerClip.length * speakerClip.channels))
+                        if ((!useVideoAsReference && referenceSpeaker.time >= speakerClip.length)
                         || (useVideoAsReference && referenceVideo.time >= (referenceVideo.length * referenceVideo.GetAudioChannelCount(0))))
                         {
 
